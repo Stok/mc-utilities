8a5c23e22a952a74f85f8b5691c9e36a4a261721
Sean Tokunaga <sean.tokunaga@imperial.ac.uk>
2013-04-17 16:57:31 +0200

    take, rms, partition, flatten, mod

--- mc-utils.js
+++ mc-utils.js
@@ -1,4 +1,3 @@
-
 //gives the rms of an array
 rms = function(x) 
 {
@@ -8,21 +7,23 @@ rms = function(x)
   {
     res += sqs[i];
   }
-  return res
+  return Math.sqrt(res / sqs.length)
 }
 
 //like take in mma
 take = function(x, y) 
 {
   res = new Array;
+  if(y.length == undefined)
+  {
   if(x.length - 1 > y)
   {
   	if(y < 0)
   	{
-      var j = 0
-  	  for(var i = x.length; i > x.length + y; i--)
+      var j = 0;
+  	  for(var i = x.length + y; i < x.length; i++)
       {
-        res[j] = x[x.length - 1 - j];
+        res[j] = x[i];
         j++;
       }
   	}
@@ -33,8 +34,50 @@ take = function(x, y)
         res[i] = x[i];
       }
   	} 
-  	if(y === 0) {return 0;}
-        } else{return 1;}
+  } else{return 1;}
+  }
+  else if(y.length == 2)
+  {
+    take(take(x, -(x.length - y[0])), y[1] - y[0]+1)
+  }
   return res
       
+}
+
+partition = function(arr, binSize)
+{
+  var numberOfParts = Math.floor(arr.length / binSize);
+  var bulk = _.range(0, numberOfParts - 1, 1).map(function(x) {return take(arr, [0 + x * binSize , binSize - 1 + x * binSize])});
+  var edge = new Array;
+  for(var i = numberOfParts * binSize; i < arr.length;  i++)
+  {
+    edge[i - numberOfParts * binSize] = arr[i];
+  }
+  return bulk.concat([edge])
+}
+
+mod= function(x, y) {return x - Math.floor(x/y)*y}
+
+flatten = function(x)
+{
+  var arr = new Array;
+  if(x.length == undefined)
+  {
+    return x;
+  }
+  else
+  {
+    for(var i = 0; i < x.length; i++)
+    {
+      if(x[i].length == undefined)
+      {
+      	arr = arr.concat([x[i]]);
+      }
+      else 
+      {
+        arr = arr.concat(flatten(x[i]));
+      }
+    }
+  }
+  return arr
 }
